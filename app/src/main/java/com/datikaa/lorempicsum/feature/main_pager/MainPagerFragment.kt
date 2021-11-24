50afafc8fefe88776c9d67573c239ebfe6614060
daniel.dattler <daniel.dattler@bcsconsult.hu>
2021-11-24 18:30:00 +0100

    Apply insets like a pro

--- app/src/main/java/com/datikaa/lorempicsum/feature/main_pager/MainPagerFragment.kt
+++ app/src/main/java/com/datikaa/lorempicsum/feature/main_pager/MainPagerFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.datikaa.lorempicsum.databinding.FragmentMainBinding
+import com.datikaa.lorempicsum.extension.doOnApplyWindowInsets
 import com.datikaa.lorempicsum.extension.onEachWithLifecycle
 import com.datikaa.lorempicsum.extension.screenMetrics
 import com.datikaa.lorempicsum.feature.main_pager.paging.MainPagerAdapter
@@ -41,8 +43,16 @@ class MainPagerFragment : Fragment() {
         onEachWithLifecycle(viewModel.flow) {
             adapter.submitData(it)
         }
+
         binding?.mainRecyclerView?.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
         binding?.mainRecyclerView?.adapter = adapter
+
+        binding?.mainRecyclerView?.doOnApplyWindowInsets { v, windowInsets, initialPadding ->
+            v.updatePadding(
+                top = initialPadding.top + windowInsets.systemWindowInsetTop,
+                bottom = initialPadding.bottom + windowInsets.systemWindowInsetBottom,
+            )
+        }
     }
 
     override fun onDestroyView() {
