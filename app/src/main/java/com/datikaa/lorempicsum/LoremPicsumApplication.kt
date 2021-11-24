d5bb4423567355a5778125b50c6b642d0a4ec3a1
daniel.dattler <daniel.dattler@bcsconsult.hu>
2021-11-24 13:58:27 +0100

    Start koin

--- app/src/main/java/com/datikaa/lorempicsum/LoremPicsumApplication.kt
+++ app/src/main/java/com/datikaa/lorempicsum/LoremPicsumApplication.kt
@@ -1,6 +1,8 @@
 package com.datikaa.lorempicsum
 
 import android.app.Application
+import com.datikaa.lorempicsum.di.viewModelsModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class LoremPicsumApplication : Application() {
@@ -8,7 +10,9 @@ class LoremPicsumApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-
+            androidContext(this@LoremPicsumApplication)
+            // declare modules
+            modules(viewModelsModule)
         }
     }
 }
--- app/src/main/java/com/datikaa/lorempicsum/di/Koin.kt
+++ app/src/main/java/com/datikaa/lorempicsum/di/Koin.kt
@@ -1,2 +1,9 @@
 package com.datikaa.lorempicsum.di
 
+import com.datikaa.lorempicsum.feature.main_pager.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelsModule = module {
+    viewModel { MainViewModel() }
+}
