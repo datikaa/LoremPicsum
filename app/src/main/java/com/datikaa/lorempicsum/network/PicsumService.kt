d357da31e6ba115246e49d404be146663af24e6b
daniel.dattler <daniel.dattler@bcsconsult.hu>
2021-11-24 14:07:06 +0100

    Add paging endpoint

--- app/src/main/java/com/datikaa/lorempicsum/network/PicsumService.kt
+++ app/src/main/java/com/datikaa/lorempicsum/network/PicsumService.kt
@@ -2,9 +2,13 @@ package com.datikaa.lorempicsum.network
 
 import com.datikaa.lorempicsum.network.response.ListItem
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PicsumService {
 
     @GET("v2/list")
     suspend fun list(): List<ListItem>
+
+    @GET("v2/list")
+    suspend fun list(@Query("page") page: Int, @Query("limit") limit: Int): List<ListItem>
 }
