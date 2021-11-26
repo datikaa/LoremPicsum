9f4183ec6d5572a44d94e8224323224059051ca0
daniel.dattler <daniel.dattler@bcsconsult.hu>
2021-11-26 12:15:31 +0100

    Add info endpoint

--- app/src/main/java/com/datikaa/lorempicsum/network/PicsumService.kt
+++ app/src/main/java/com/datikaa/lorempicsum/network/PicsumService.kt
@@ -2,6 +2,7 @@ package com.datikaa.lorempicsum.network
 
 import com.datikaa.lorempicsum.network.response.PicsumResponseItem
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PicsumService {
@@ -11,4 +12,7 @@ interface PicsumService {
 
     @GET("v2/list")
     suspend fun list(@Query("page") page: Int, @Query("limit") limit: Int): List<PicsumResponseItem>
+
+    @GET("v2/id/{id}/info")
+    suspend fun info(@Path("id") id: Int): PicsumResponseItem
 }
