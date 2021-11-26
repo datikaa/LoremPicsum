2c25a1a51e47706b01cad60a719411c59de0f4a6
daniel.dattler <daniel.dattler@bcsconsult.hu>
2021-11-26 19:02:09 +0100

    Theming touches

--- app/src/main/java/com/datikaa/lorempicsum/feature/picture_detail/DetailsFragment.kt
+++ app/src/main/java/com/datikaa/lorempicsum/feature/picture_detail/DetailsFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.util.Linkify
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
